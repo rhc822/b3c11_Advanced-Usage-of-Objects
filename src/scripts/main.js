2372089585b8e1484b16cc2c33701452445e8341
rhc822 <rhc822@gmail.com>
2019-11-04 08:55:07 -0600

    added reference to index file and content from chapter

---
+++ src/scripts/main.js
@@ -0,0 +1,44 @@
+const allCars = [
+    {
+        model: "Mustang",
+        color: "Midnight Blue",
+        year: 1976,
+        length: 120,
+        width: 62,
+        height: 47
+    },
+    {
+        model: "Xterra",
+        color: "Forest Green",
+        year: 2011,
+        length: 144,
+        width: 71,
+        height: 55
+    },
+    {
+        model: "Thunderbird",
+        color: "Red",
+        year: 2005,
+        length: 115,
+        width: 58,
+        height: 42
+    },
+    {
+        model: "Suburban",
+        color: "Grey",
+        year: 2015,
+        length: 149,
+        width: 73,
+        height: 58
+    }
+]
+
+// Dot notation
+console.log(mustang.color)
+
+// Square bracket notation
+console.log(mustang["color"])
+
+// Dynamic square bracket notation
+const colorProperty = "color"
+console.log(mustang[colorProperty])
